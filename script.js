1e492650d23b5a11f22aebccd49ca845ce2ea1d0
UMM RASLAN <umutesimariam04@gmail.com>
2026-03-25 09:48:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Simple animation on scroll
+window.addEventListener("scroll", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    const position = card.getBoundingClientRect().top;
+    const screen = window.innerHeight;
+
+    if (position < screen - 100) {
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
